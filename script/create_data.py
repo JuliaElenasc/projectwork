be39748ffdc8950715df03e161af0236ddfdf79f
Giuseppe Base <g.base@itsrizzoli.it>
2023-09-14 15:45:48 +0200

    migliorato file per pulire i dati geografici e calcolare la matrice delle distanze

--- script/create_data.py
+++ script/create_data.py
@@ -1,36 +1,41 @@
 import geopandas as gpd
 import libpysal
+import igraph
 import networkx as nx
+import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
     city_to_remove = ['Campione d\'Italia', 'Capraia Isola', 'Isola del Giglio', 'Ponza', 'Ventotene', 'Procida',
                       'Isole Tremiti', 'Favignana', 'Pantelleria', 'Ustica', 'Lipari', 'Lampedusa e Linosa',
-                      'La Maddalena', 'Carloforte']
-    path_city_shape = '../../dati/Com01012023_WGS84.shp'
+                      'La Maddalena', 'Carloforte', 'Gorgona Scalo', 'Marettimo', 'Levanzo']
+    path_city_shape = '../dati/Limiti01012023/Limiti01012023/Com01012023/Com01012023_WGS84.shp'
+    # leggo il file
+    print('leggo il file')
     city_shape = gpd.read_file(path_city_shape)
+    # tolgo le isole e le citta che non ci interessano
+    print('tolgo le isole e le citta che non ci interessano')
     city_shape = city_shape[~city_shape['COMUNE'].isin(city_to_remove)]
-    city_shape['centroid'] = city_shape['geometry'].centroid
-    city_nb = libpysal.weights.Queen.from_dataframe(city_shape)
-    distances = city_shape['centroid'].apply(lambda x: city_shape['centroid'].distance(x))
-    Graph_distance = nx.Graph()
-    for i in range(len(distances.index)):
-        for j in range(i + 1, len(distances.columns)):
-            distance = distances.iloc[i, j]
-            Graph_distance.add_edge(distances.index[i], distances.columns[j], weight=distance)
-    Graph_df = pd.DataFrame(nx.to_pandas_edgelist(Graph_distance), columns=['source', 'target', 'weight'])
-    Graph_df.to_pickle('../dati/distances.pkl')
-    fig, ax = plt.subplots(figsize=(12, 12))
-    city_shape.plot(ax=ax, alpha=0.7)
-
-    pos = {node: (city_shape['centroid'].loc[node].x, city_shape['centroid'].loc[node].y) for node in
-           Graph_distance.nodes()}
-    labels = {node: node for node in Graph_distance.nodes()}
-    weights = [Graph_distance[edge[0]][edge[1]]['weight'] for edge in Graph_distance.edges()]
-
-    nx.draw(Graph_distance, pos, labels=labels, width=weights, edge_color='b', ax=ax)
-
-    plt.axis('off')
-    plt.title("Grafo delle Distanze tra Comuni")
-    plt.show()
+    # mi estrapolo la lista delle citta
+    print('mi estrapolo la lista delle citta')
+    city_list = list(city_shape['COMUNE'])
+    num_cities = len(city_list)
+    # calcolo dei vicini
+    print('calcolo dei vicini')
+    city_weights = libpysal.weights.Queen.from_dataframe(city_shape)
+    # calcolo centroidi
+    print('calcolo centroidi')
+    city_centroid = city_shape.centroid
+    # matrice di adiacenza
+    print('matrice di adiacenza')
+    cities_matrix = libpysal.weights.full(city_weights)[0]
+    # matrice distanze
+    print('matrice distanze')
+    graph_city = nx.Graph(cities_matrix)
+    print('errore passato')
+    distance_matrix = nx.all_pairs_shortest_path_length(graph_city)
+    output = pd.DataFrame(distance_matrix)
+    print('fine')
+    output.to_pickle('../dati/distance_matrix.pkl')
+    print('vittoria')
