2a4ee5c68768edfa8ba864c85ee4339e504cd1f4
JuliaElenaSC <julia.elena.sc@gmail.com>
2023-10-03 22:21:23 +0200

    Clean merge aggiornato (print finale)

--- script/clean_merge_csv.py
+++ script/clean_merge_csv.py
@@ -1,30 +1,25 @@
-#Aprire file proporzionati
 import geopandas as gpd
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import config
-import os
 
 
-def clean_data (path_csv_mercato, path_shapefile, output_dir):
-    
-    shapefile_comune = path_shapefile
+def clean_data(path_csv_mercato: str, path_shapefile: str, output_dir: str):
+    print('Iniziando pulizia dei dati')
     csv_mercati = pd.read_csv(path_csv_mercato)
-    
-    print("Reading input files, please wait...")
-        
-    # Delete data
-    print("Cleaning data and obtaining the city code...")
-    comune_del= ["Campione d'Italia", "Capraia Isola", "Isola del Giglio", "Ponza", "Ventotene", "Procida", "Isole Tremiti", "Favignana", "Pantelleria", "Ustica", "Lipari", "Lampedusa e Linosa", "La Maddalena", "Carloforte"]
-    
-    # Open the shapefile
-    gdf = gpd.read_file(shapefile_comune, encoding='utf-8')
+
+    # Lista dei comuni da rimuovere
+    comune_del = ["Campione d'Italia", "Capraia Isola", "Isola del Giglio", "Ponza", "Ventotene", "Procida",
+                  "Isole Tremiti", "Favignana", "Pantelleria", "Ustica", "Lipari", "Lampedusa e Linosa", "La Maddalena",
+                  "Carloforte"]
+
+    # Apertura del file e rimozione dei comuni
+    gdf = gpd.read_file(path_shapefile, encoding='utf-8')
     gdf = gdf[~gdf['COMUNE'].isin(comune_del)]
-      
-    # Correct outdated names in the CSV
-    csv_mercati['Comune_residenza'] = csv_mercati['Comune_residenza'].replace('Bardello', 'Bardello con Malgesso e Bregano')
-    csv_mercati['Comune_residenza'] = csv_mercati['Comune_residenza'].replace('Bregano', 'Bardello con Malgesso e Bregano')
+
+    # Correzione dei dati nel csv
+    csv_mercati['Comune_residenza'] = csv_mercati['Comune_residenza'].replace('Bardello',
+                                                                              'Bardello con Malgesso e Bregano')
+    csv_mercati['Comune_residenza'] = csv_mercati['Comune_residenza'].replace('Bregano',
+                                                                              'Bardello con Malgesso e Bregano')
     csv_mercati['Comune_residenza'] = csv_mercati['Comune_residenza'].replace('Tonengo', 'Moransengo-Tonengo')
     csv_mercati['Comune_residenza'] = csv_mercati['Comune_residenza'].replace('Vendrogno', 'Bellano')
     csv_mercati['Comune_residenza'] = csv_mercati['Comune_residenza'].replace('Riva Valdobbia', 'Alagna Valsesia')
@@ -34,25 +29,22 @@ def clean_data (path_csv_mercato, path_shapefile, output_dir):
     csv_mercati['Comune_residenza'] = csv_mercati['Comune_residenza'].replace('Castellar', "Saluzzo")
     csv_mercati['Comune_residenza'] = csv_mercati['Comune_residenza'].replace('Camo', "Santo Stefano Belbo")
     csv_mercati['Comune_residenza'] = csv_mercati['Comune_residenza'].replace("Ca' d'Andrea", "Torre de' Picenardi")
-    
-    # Group i dati csv 
-    data_group = group = csv_mercati.groupby('Comune_residenza').mean().reset_index()
- 
-    # Merge CSV e Shapefile
-    total_data= pd.DataFrame(pd.merge(gdf, data_group, left_on='COMUNE', right_on= 'Comune_residenza', how= 'outer'))
-   
-    # Delete empty data and data not found in the shapefile
+    # Raggruppamento dei dati del csv
+    data_group = csv_mercati.groupby('Comune_residenza').mean(numeric_only=True).reset_index()
+
+    # Unione tra lo shapefile e il csv
+    total_data = pd.DataFrame(pd.merge(gdf, data_group, left_on='COMUNE', right_on='Comune_residenza', how='outer'))
+
+    # Rimozione dei dati vuoti o mancanti dello shapefile
     total_data_cleaned = total_data.dropna(subset=['COMUNE'])
-    
-    columns_delete= ['COD_RIP','COD_REG', 'COD_PROV','COD_CM','COD_UTS','COMUNE','COMUNE_A','CC_UTS','SHAPE_LENG','Shape_Le_1', 'geometry']
+
+    columns_delete = ['COD_RIP', 'COD_REG', 'COD_PROV', 'COD_CM', 'COD_UTS', 'COMUNE', 'COMUNE_A', 'CC_UTS',
+                      'SHAPE_LENG', 'Shape_Le_1', 'geometry']
     total_data_cleaned = total_data_cleaned.drop(columns_delete, axis=1)
-  
-    # Export data to a new CSV
-    name_output_file= 'Mercato_code.csv'
-    result = os.path.join(output_dir,name_output_file)
-    
-    total_data_cleaned.to_csv(result, index=False)
-    print (f'File saved in {result}')
+
+    # Salvataggio dei dati
+    total_data_cleaned.to_csv(output_dir, index=False)
+    print('Dati puliti salvati nella cartella output')
 
 
 
